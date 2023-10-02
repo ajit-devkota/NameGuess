e880603ec2eeb624b2957ed7f23f5836f740848c
ajit-devkota <143102899+ajit-devkota@users.noreply.github.com>
2023-10-02 15:23:50 +0300

    first commit

---
+++ guess.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class guess {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String correctAnswer = "Emma";
+		String guess = " ";
+		int attempts = 0;
+		do {
+			System.out.println("Guess my name (type stop to exit)");
+			guess = in.nextLine();
+
+			if (!guess.equals("stop")) {
+				attempts++;
+			}
+			if (guess.equals(correctAnswer)) {
+				System.out.println("Congratulations");
+				break;
+				
+			}
+			if (guess.equals("stop")) 
+				
+			break;
+		}while(true);
+		System.out.println("You guessed "+ attempts + " times.");
+		
+	}
+}
